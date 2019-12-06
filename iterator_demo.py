387b626a5c723fe31a3fff1df204bf55e212a6d3
appyens <an.anurag@msn.com>
2019-12-07 02:48:04 +0530

    hackerrank added

--- iterator_demo.py
+++ iterator_demo.py
@@ -16,4 +16,11 @@ def gen():
 
 
 a = iter(gen())
-print(next(a))
+# print(next(a))
+
+# b = iter(int, 1)
+#
+# for i in b:
+#     print(i)
+
+print(int())
--- resource/notes/syllabus.py
+++ resource/notes/syllabus.py
@@ -284,7 +284,8 @@
             keys()	Returns a list containing the dictionary's keys
             pop()	Removes the element with the specified key
             popitem()	Removes the last inserted key-value pair
-            setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+            setdefault()	Returns the value of the specified key. If the key does not exist:
+                            insert the key, with the specified value
             update()	Updates the dictionary with the specified key-value pairs
             values()	Returns a list of all the values in the dictionary
     14.None
