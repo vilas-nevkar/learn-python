88ab950aa8f9ea65dfdd153d5e0172519fd56e31
appyens <an.anurag@msn.com>
2019-12-09 00:47:37 +0530

    notes updated

--- resource/notes/syllabus.py
+++ resource/notes/syllabus.py
@@ -522,14 +522,33 @@ File Handling
         2. with open()
 
     2. Access modes
-            'r'	This is the default mode. It Opens file for reading.
-            'w'	This Mode Opens file for writing. If file does not exist, it creates a new file.
-                If file exists it truncates the file.
+
+             ``r''   Open text file for reading.  The stream is positioned at the
+                     beginning of the file.
+
+             ``r+''  Open for reading and writing.  The stream is positioned at the
+                     beginning of the file.
+
+             ``w''   Truncate file to zero length or create text file for writing.
+                     The stream is positioned at the beginning of the file.
+
+             ``w+''  Open for reading and writing.  The file is created if it does not
+                     exist, otherwise it is truncated.  The stream is positioned at
+                     the beginning of the file.
+
+             ``a''   Open for writing.  The file is created if it does not exist.  The
+                     stream is positioned at the end of the file.  Subsequent writes
+                     to the file will always end up at the then current end of file,
+                     irrespective of any intervening fseek(3) or similar.
+
+             ``a+''  Open for reading and writing.  The file is created if it does not
+                     exist.  The stream is positioned at the end of the file.  Subse-
+                     quent writes to the file will always end up at the then current
+                     end of file, irrespective of any intervening fseek(3) or similar.
+
             'x'	Creates a new file. If file already exists, the operation fails.
-            'a'	Open file in append mode. If file does not exist, it creates a new file.
             't'	This is the default mode. It opens in text mode.
             'b'	This opens in binary mode.
-            '+'	This will open a file for reading and writing (updating)
 
     3. Methods
             close()	Close an open file. It has no effect if the file is already closed.
