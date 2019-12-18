4fc9a56844a76b5d6e1977a86aab24c5cfade7bb
capricorn <an.anurag@msn.com>
2019-12-18 18:26:34 +0530

    syllabus md added

---
+++ resource/notes/web_syllabus.py
@@ -0,0 +1,34 @@
+"""
+Front end
+    Fundamentals:
+        Rendering
+        User management
+            Roles
+            Permission
+            Admin
+            Users
+        Password management
+        Pagination
+        Sitemap
+        RSS
+        Detail view
+        List view
+        Mail sending
+        Infinite scroll
+        Local search
+        Forms
+        Tables
+        Tagging
+        Async task
+
+    Social / Blog::
+        post
+        comment
+        like
+        share
+        Notification
+    Management:
+Backend
+
+Database
+"""
